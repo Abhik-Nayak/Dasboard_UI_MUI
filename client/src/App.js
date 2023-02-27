826f9b34205ecfa6450f5eb937806a64e8ee0746
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-02-27 09:52:49 +0530

    Abhik/27-02-2023

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,51 @@
-import React, { useState } from 'react';
-import Sidebar1 from './components/Sidebar1';
-import Feed from './components/Feed';
-import Rightbar from './components/Rightbar';
-import Navbar from "./components/Navbar";
-import Add from './components/Add';
-import { Box, Container, Drawer, Stack, ThemeProvider, createTheme } from "@mui/material";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
-const App = () => {
-  const [mode, setMode] = useState("light");
-  const [open1,setOpen1] = useState("true");
-  const darkTheme = createTheme({
-    palette: {
-      mode: mode
-    }
-  })
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={"background.default"} color={"text.primary"}>
-        <Navbar setOpen1={setOpen1} open1={open1}/>
-        <Stack direction="row" spacing={2} justifyContent="space-between">
-          <Sidebar1 setMode={setMode} mode={mode} open1={open1}/>
-          <Feed />
-          <Rightbar />
-        </Stack>
-      </Box>
-    </ThemeProvider>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
